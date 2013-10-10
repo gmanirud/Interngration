699155be9e14bc58c074dd3277117a69b92943cc
Hargun Suri <Hargun.Suri@Xtremelabs.com>
2013-10-09 20:14:46 -0400

    HRS - Revison 9-oct-2013

--- studentDeleteMessage.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$delpid=$_GET['delpid'];
-        include "config.php"; 	
-		$ses_result=mysql_select_db($dbname) or die(mysql_error());
-		$sqlUpdate ="Update student_mailbox set flg_delete='1' where id='$delpid'";
-	    $result=mysql_query($sqlUpdate);
-		header("Location:studentInbox.php");
-		?>
